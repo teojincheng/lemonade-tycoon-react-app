4d10cb9275e7ff2c5501a398283c7fd8a5d2894a
Teo Jin Cheng <jinchengproject@gmail.com>
2020-01-11 10:57:17 +0800

    updated test file to include shallow rendering

--- src/Counter.test.js
+++ src/Counter.test.js
@@ -1,16 +1,28 @@
-import React from 'react';
-import Counter from './Counter';
+import React from "react";
+import Counter from "./Counter";
+import Adapter from "enzyme-adapter-react-16";
+import { shallow, mount, render, configure } from "enzyme";
 
-test('test update value of counter', () => {
-    let c1 = new Counter();
-    c1.update(78);
-    expect(c1.getValue()).toBe(78);
+/*
+
+var c1 = new Counter();
+c1.update(188);
+renderer.render(c1.displayValue());
+
+*/
+configure({ adapter: new Adapter() });
+var c1 = new Counter();
+c1.update(188);
+
+describe("check displayValue() method", () => {
+  it("renders a div", () => {
+    const wrapper = shallow(c1.displayValue());
+    expect(wrapper.contains(<div>{188}</div>)).toBe(true);
   });
+});
 
-
-  test('test displayValue() function',() => {
-     let c1 = new Counter();
-     c1.update(188);
-     let expectedElement = <div>188</div>
-     expect(c1.displayValue().toString()).toBe(expectedElement);
-  });
+test("test update value of counter", () => {
+  let c1 = new Counter();
+  c1.update(78);
+  expect(c1.getValue()).toBe(78);
+});
