21323dcd301eaea5977798837f078e2aa9578f56
teojincheng <jinchengproject@gmail.com>
2020-03-08 14:39:36 +0800

    update spec file to pass test

--- cypress/integration/tyc.spec.js
+++ cypress/integration/tyc.spec.js
@@ -1,7 +1,7 @@
 describe("tycoon", () => {
   it("should visit the webpage of the app and header 'supplies' is visible on the page", () => {
     cy.visit("https://vigorous-hugle-ee108f.netlify.com");
-    cy.get(".card-default h2").contains("Supplies");
+    cy.get(".card-default h3").contains("Supplies");
   });
 
   it("should click on the plus button for lemon and supplies of lemon should become one", () => {
